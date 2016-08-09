ce174fc8d8da2f25d1575505c775e2a995eb369a
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-08-09 06:56:34 -0600

    got a nested version of ajax calls somewhat working

--- src/js/characterSearch.js
+++ src/js/characterSearch.js
@@ -10,57 +10,34 @@ $('form').on('submit', function(event) {
   var char1 = encodeURI($('#character1').val());
   var char2 = encodeURI($('#character2').val());
 
-  getList(char1, char2);
+  $.ajax({
+    url:'https://gateway.marvel.com/v1/public/characters?name=' + char1 + '&apikey=f0807a37bd4542fa4a26ada4b33c8f5d',
+    method: 'GET'
+  }).done(function(char1Info) {
+    var idChar1 = char1Info.data.results[0].id;
+    console.log(idChar1);
 
+    $.ajax({
+      url:'https://gateway.marvel.com:443/v1/public/characters/' + idChar1 + '/comics?apikey=f0807a37bd4542fa4a26ada4b33c8f5d',
+      method: 'GET'
+    }).done(function(char1Comics) {
+      var resultsArray = char1Comics.data.results;
+      resultsArray.forEach(function(eachComic) {
+        console.log(eachComic);
+        $('.results-list').append('<div class="row"><h5><img src="' + eachComic.thumbnail.path + '/portrait_medium.jpg"> ' + eachComic.title + '</h5><p>' + eachComic.description + '</p></div>');
+      });
+    });
+  });
 });
 
-function getId(name) {
-
-  var charName = encodeURI(name);
-
-  $.ajax({
-    url:'https://gateway.marvel.com/v1/public/characters?name=' + charName + '&apikey=f0807a37bd4542fa4a26ada4b33c8f5d',
-    method: 'GET'
-  }).done(function(charInfo) {
-    var charId = charInfo.data.results[0].id;
-    return charId;
-  });
-}
-
-var idArray = [];
-
-function getList(name1, name2) {
-
-  getId(name1)
-    .then(function(id) {
-    idArray.push(id);
-    return getId(name2);
-  })
-    .then(function(id) {
-    idArray.push(id);
-  })
-    .then(function() {
-      console.log(idArray);
-      $.ajax({
-        url: 'https://gateway.marvel.com:443/v1/public/characters/' + idArray[0] + '/comics?sharedAppearances=' + idArray[1] + '&limit=100&apikey=f0807a37bd4542fa4a26ada4b33c8f5d',
-        method: 'GET'
-      }).done(function(sharedComics) {
-        var resultsArray = sharedComics.data.results;
-        console.log(resultsArray);
-      });
-  });
-}
-
+  // https://gateway.marvel.com:443/v1/public/characters/1009718/comics?orderBy=onsaleDate&apikey=f0807a37bd4542fa4a26ada4b33c8f5d
 
   // $.ajax({
-  //   url: 'https://gateway.marvel.com:443/v1/public/characters/' + id1 + '/comics?sharedAppearances=' + id2 + '&limit=100&apikey=f0807a37bd4542fa4a26ada4b33c8f5d',
+  //   url:'https://gateway.marvel.com/v1/public/characters?name=' + char2 + '&apikey=f0807a37bd4542fa4a26ada4b33c8f5d',
   //   method: 'GET'
-  // }).done(function(sharedComics) {
-  //   var resultsArray = sharedComics.data.results;
-  //   console.log(resultsArray);
-    // resultsArray.forEach(function(eachComic) {
-    //   console.log(eachComic);
-    //   $('.results-list').append('<div class="row"><h5><img src="' + eachComic.thumbnail.path + '/portrait_medium.jpg"> ' + eachComic.title + '</h5><p>' + eachComic.description + '</p></div>');
-    // });
-//   });
-// }
+  // }).done(function(char2Info) {
+  //   console.log(char2Info);
+  // });
+  //http://gateway.marvel.com:80/v1/public/characters?name=Wolverine&apikey=f0807a37bd4542fa4a26ada4b33c8f5d
+
+  //https://gateway.marvel.com:443/v1/public/characters?name=wolverine&apikey=f0807a37bd4542fa4a26ada4b33c8f5d
