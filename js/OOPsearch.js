4b469efe945cb8002ea642725a2b90437057bdaa
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-08-09 12:04:49 -0600

    nested search working now needs 4 ajax calls to get list

---
+++ js/OOPsearch.js
@@ -0,0 +1,90 @@
+$(document).on('ready', function() {
+  console.log('sanity check!');
+});
+
+$('form').on('submit', function(event) {
+  event.preventDefault();
+
+  var char1 = encodeURI($('#character1').val());
+  var char2 = encodeURI($('#character2').val());
+
+  charSearch(char1, char2);
+
+});
+
+function charSearch(name1, name2) {
+
+  $.ajax({
+    url:'https://gateway.marvel.com/v1/public/characters?name=' + name1 + '&apikey=f0807a37bd4542fa4a26ada4b33c8f5d',
+    method: 'GET'
+  }).done(function(char1Info) {
+    var idChar1 = char1Info.data.results[0].id;
+    console.log(idChar1);
+
+    $.ajax({
+      url:'https://gateway.marvel.com:443/v1/public/characters/' + idChar1 + '/comics?format=comic&formatType=comic&noVariants=true&limit=100&apikey=f0807a37bd4542fa4a26ada4b33c8f5d',
+      method: 'GET'
+    }).done(function(char1Comics) {
+      var char1List = char1Comics.data.results;
+      console.log(char1List);
+
+      $.ajax({
+        url:'https://gateway.marvel.com/v1/public/characters?name=' + name2 + '&apikey=f0807a37bd4542fa4a26ada4b33c8f5d',
+        method: 'GET'
+      }).done(function(char2Info) {
+        var idChar2 = char2Info.data.results[0].id;
+        console.log(idChar2);
+
+        $.ajax({
+          url:'https://gateway.marvel.com:443/v1/public/characters/' + idChar2 + '/comics?format=comic&formatType=comic&noVariants=true&limit=100&apikey=f0807a37bd4542fa4a26ada4b33c8f5d',
+          method: 'GET'
+        }).done(function(char2Comics) {
+          var char2List = char2Comics.data.results;
+          console.log(char2List);
+
+          char1List.forEach(function(result1) {
+            char2List.forEach(function(result2) {
+              if (result1.id === result2.id) {
+                $('.results-list').append('<div class="row"><img src="' + result1.thumbnail.path + '/portrait_uncanny.jpg"><h5>' + result1.title + '</h5><p>' + result1.description + '</p></div>');
+              }
+            });
+          });
+
+        });
+
+      });
+
+    });
+
+  });
+}
+
+// function twoCharSearch(name1, name2) {
+//   $.ajax({
+//     url:'https://gateway.marvel.com/v1/public/characters?name=' + name1 + '&apikey=f0807a37bd4542fa4a26ada4b33c8f5d',
+//     method: 'GET'
+//   }).done(function(char1Info) {
+//     var idChar1 = char1Info.data.results[0].id;
+//     console.log(idChar1);
+//
+//     $.ajax({
+//       url:'https://gateway.marvel.com/v1/public/characters?name=' + name2 + '&apikey=f0807a37bd4542fa4a26ada4b33c8f5d',
+//       method: 'GET'
+//     }).done(function(char2Info) {
+//       var idChar2 = char2Info.data.results[0].id;
+//       console.log(idChar2);
+//
+//       $.ajax({
+//         url:'https://gateway.marvel.com:443/v1/public/characters/' + idChar1 + '/comics?sharedAppearances=' + idChar2 + '&limit=100&apikey=f0807a37bd4542fa4a26ada4b33c8f5d',
+//         method: 'GET'
+//       }).done(function(sharedComics) {
+//         console.log(sharedComics);
+//         var resultsArray = sharedComics.data.results;
+//         resultsArray.forEach(function(eachComic) {
+//           console.log(eachComic);
+//           $('.results-list').append('<div class="row"><h5><img src="' + eachComic.thumbnail.path + '/portrait_medium.jpg"> ' + eachComic.title + '</h5><p>' + eachComic.description + '</p></div>');
+//         });
+//       });
+//     });
+//   });
+// }
