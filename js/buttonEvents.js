0b01793ec07b41e0763670a4c20846bfa3714f0f
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-08-12 10:19:34 -0600

    toggle date buttons now works

--- js/buttonEvents.js
+++ js/buttonEvents.js
@@ -5,13 +5,51 @@ $(document).on('ready', function() {
 $('.dateButton').on('click', function(event) {
   event.preventDefault();
 
-  $('.dateChoices').append('<input type="checkbox" name="dates" id="goldenAge" value="goldenAge"/>Golden Age (1938-1955)<input type="checkbox" name="dates" id="silverAge" value="silverAge"/>Silver Age (1956-1970)<br><input type="checkbox" name="dates" id="bronzeAge" value="bronzeAge"/>Bronze Age (1971-1985)<input type="checkbox" name="dates" id="modernAge" value="modernAge"/>Modern Age (1986-present)<span class="custom-date"><input type="checkbox" name="dates" id="customRange" value="customRange"/>Custom Dates:</span><div class="row date-inputs">Start Date:<input type="date" id="startDate"><br>End Date:<input type="date" id="endDate"></div>');
+  $('.dateChoices').prepend('<button type="button" name="button" class="removeDate">Remove Date Range</button><br>');
+
+  $('.dateChoices').append('<input type="checkbox" name="dates" id="goldenAge" value="goldenAge"/>Golden Age (1938-1955)<input type="checkbox" name="dates" id="silverAge" value="silverAge"/>Silver Age (1956-1970)<br><input type="checkbox" name="dates" id="bronzeAge" value="bronzeAge"/>Bronze Age (1971-1985)<input type="checkbox" name="dates" id="modernAge" value="modernAge"/>Modern Age (1986-present)<input type="checkbox" name="dates" id="customRange" value="customRange"/>Custom Dates:<div class="row date-inputs">Start Date:<input type="date" id="startDate"><br>End Date:<input type="date" id="endDate"></div>');
 
   $('.dateButton').remove();
 
 });
 
+// function addDateButton() {
+//
+//   counter++;
+//
+//   if (counter === 1) {
+//     $('.dateRange').append('<button type="button" name="button" class="addDate">Add Date Range for Results?</button><br>');
+//   }
+// }
+
+$(document).on('click', '.removeDate', function(event) {
+  event.preventDefault();
+
+  $('.dateChoices').hide();
+
+  $('.removeDate').remove();
+
+  $('.dateRange').append('<button type="button" name="button" class="addDate">Add Date Range for Results?</button>');
+
+
+});
+
+$(document).on('click', '.addDate', function(event) {
+  event.preventDefault();
+
+  $('.dateChoices').show();
+
+  $('.addDate').remove();
+
+  $('.dateChoices').prepend('<button type="button" name="button" class="removeDate">Remove Date Range</button>');
+
+});
+
+
+
+
+
+
 $(document).on("click", 'input[type="checkbox"]', function() {
-    $(this).siblings(":checked").removeAttr('checked');
-    // $('input[type="checkbox"]').hasAttr("checked").removeAttr('checked');
+  $(this).siblings(":checked").removeAttr('checked');
 });
